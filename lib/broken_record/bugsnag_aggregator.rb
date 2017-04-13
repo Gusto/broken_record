9a3001487ca86dfb33c902df0b6b16ce85c6a9ac
Nikhil Mathew <nikhilmat@gmail.com>
2017-04-12 18:54:38 -0700

    Fix bugsnag calls, make synchronous

--- lib/broken_record/bugsnag_aggregator.rb
+++ lib/broken_record/bugsnag_aggregator.rb
@@ -8,6 +8,7 @@ module BrokenRecord
     MAX_IDS = 500
 
     def report_job_start
+      configure_bugsnag!
       notify_deploy
     end
 
@@ -96,28 +97,24 @@ module BrokenRecord
     end
 
     def notify(exception, options)
-      Bugsnag.notify(exception, default_bugsnag_options.merge(options))
+      Bugsnag.notify(exception, options)
     end
 
     def notify_deploy
       Bugsnag::Deploy.notify(
-        default_bugsnag_options.merge(
-          repository: ENV['BROKEN_RECORD_REPOSITORY'],
-          branch: ENV['BROKEN_RECORD_BRANCH']
-        )
+        repository: ENV['BROKEN_RECORD_REPOSITORY'],
+        branch: ENV['BROKEN_RECORD_BRANCH']
       )
     end
 
-    def default_bugsnag_options
-      @opts ||= begin
-        raise 'Bugsnag API Key must be set!' unless BrokenRecord::Config.bugsnag_api_key
-        
-        {
-          api_key: BrokenRecord::Config.bugsnag_api_key,
-          notify_release_stages: ['development'],
-          app_version: Date.today.to_s,
-          app_type: 'validation'
-        }
+    def configure_bugsnag!
+      raise 'Bugsnag API Key must be set!' unless BrokenRecord::Config.bugsnag_api_key
+      Bugsnag.configure do |c|
+        c.notify_release_stages = ['development']
+        c.api_key = BrokenRecord::Config.bugsnag_api_key
+        c.app_version = Date.today.to_s
+        c.app_type = 'validation'
+        c.delivery_method = :synchronous
       end
     end
   end
