05b79eb302ad6b93d1834d83300129a6c683b8ee
Alex Evanczuk <alexevanczuk@gmail.com>
2017-08-05 15:09:01 -0700

    Have JsonAggregator use ReportableError

--- lib/broken_record/json_aggregator.rb
+++ lib/broken_record/json_aggregator.rb
@@ -2,18 +2,22 @@ require 'json'
 
 module BrokenRecord
   class JsonAggregator < ResultAggregator
-    
+
     FILENAME = 'broken_record_results.json'
-    
+
     def report_final_results
       json = @aggregated_results.reduce({}) do |acc, (klass, job_results)|
         acc[klass.name] = {
           duration: duration(klass),
-          invalid_records: job_results.map(&:normalized_errors).reject(&:empty?)
+          invalid_records: job_results.map(&:all_errors).map{|errors| errors.map(&:normalized_error)}
         }
         acc
       end
 
+      write_json(json)
+    end
+
+    def write_json(json)
       File.open(FILENAME, 'w') { |f| f.puts(JSON.generate(json)) }
     end
   end
--- spec/lib/broken_record/json_aggregator_spec.rb
+++ spec/lib/broken_record/json_aggregator_spec.rb
@@ -1,60 +1,41 @@
 module BrokenRecord
   describe JsonAggregator do
     let(:json_aggregator) { JsonAggregator.new }
-    let(:object_result0) { JobResult.new(Job.new(klass:Object)) }
-    let(:object_result1) { JobResult.new(Job.new(klass:Object)) }
-    let(:object_result1) { JobResult.new(Job.new(klass:Object)) }
-    let(:object_result1) { JobResult.new(Job.new(klass:Object)) }
-    let(:string_result0) { JobResult.new(Job.new(klass:String)) }
-    let(:string_result1) { JobResult.new(Job.new(klass:String)) }
-    let(:json_file) { 'broken_record_results.json' }
+
     describe '#report_final_results' do
-      before(:each) do
-        object_result0.start_timer
-        object_result0.stop_timer
-        object_result0.add_error(id: 1, error_type: 'Invalid Record', message: 'Missing Title')
+      let(:aggregator) { json_aggregator }
+      include_context 'aggregator setup'
 
-        object_result1.start_timer
-        object_result1.stop_timer
-        object_result1.add_error(id: 0, error_type: 'Exception', message: 'Something is wrong')
-
-        string_result0.start_timer
-        string_result0.stop_timer
-        string_result0.add_error(error_type: 'Loading Error', message: 'Whaaaat!')
-
-        string_result1.start_timer
-        string_result1.stop_timer
-
-        json_aggregator.add_result(object_result0)
-        json_aggregator.add_result(object_result1)
-        json_aggregator.add_result(string_result0)
-        json_aggregator.add_result(string_result1)
-      end
+      subject { json_aggregator.report_final_results }
 
       it 'creates a json file with formatted errors' do
-        expect(File).to_not exist(json_file)
-
-        json_aggregator.report_final_results
         errors = {
           'Object'=> {
-            'duration'=>0.0,
-            'invalid_records'=>[
-              [{'id'=>1, 'message'=>'Missing Title', 'error_type'=>'Invalid Record'}],
-              [{'id'=>0, 'message'=>'Something is wrong', 'error_type'=>'Exception'}]
+            duration: 25.0,
+            invalid_records: [
+              [
+                {id: 1, message: 'invalid Object model 1', error_title: 'Invalid Record'}
+              ],
+              [
+                {id: 2, message: 'invalid Object model 2', error_title: 'Invalid Record'}
+              ]
             ]
           },
           'String'=> {
-            'duration'=>0.0,
-            'invalid_records'=>[
-              [{'id'=>nil, 'message'=>'Whaaaat!', 'error_type'=>'Loading Error'}]
+            duration: 0.234,
+            invalid_records: [
+              [
+                {id: 3, message: 'invalid String model 3', error_title: 'Invalid Record'},
+                {id: 4, message: 'invalid String model 4', error_title: 'Validation Exception'},
+                {id: 5, message: 'invalid String model 5', error_title: 'Loading Error'}
+              ],
             ]
           }
         }
-        expect(File).to exist(json_file)
-        File.open(json_file).each do |line|
-          expect(JSON.parse(line)).to eql(errors)
-        end
-        File.delete(json_file)
+
+        expect(json_aggregator).to receive(:write_json).with(errors)
+        subject
+
       end
     end
   end
