5630159bc2c73be62e2898fcb61a2080a87d8e13
Alex Evanczuk <alexevanczuk@gmail.com>
2017-08-28 12:52:33 -0700

    Revert "Add code coverage"
    This reverts commit df893e346ae94cc8e1d1f9acbf27f30dca879263.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,3 @@
-require 'coveralls'
-require 'simplecov'
-
-formatters = [SimpleCov::Formatter::HTMLFormatter]
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
-SimpleCov.start do
-  add_filter 'spec'
-end
-
 require 'rspec'
 require 'awesome_print'
 require 'yaml'
