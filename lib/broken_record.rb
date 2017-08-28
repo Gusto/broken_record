db269aa2d88a41831343cece6693cc0ef86ca44b
Alex Evanczuk <alexevanczuk@gmail.com>
2017-08-28 12:58:28 -0700

    Explicitly call deprecation warn functions

--- lib/broken_record.rb
+++ lib/broken_record.rb
@@ -19,30 +19,33 @@ module BrokenRecord
 
     unless BrokenRecord::Config.aggregator_class.nil?
       warn_class_string_deprecation('aggregator_class')
+      ActiveSupport::Deprecation.warn(
+        "aggregator_class is deprecated and will be removed in the next major version.
+        Please use aggregator, e.g. :console"
+      )
       BrokenRecord::Config.aggregator = get_symbol_from_klass_string(BrokenRecord::Config.aggregator_class)
     end
 
     unless BrokenRecord::Config.multi_aggregator_classes.nil? || BrokenRecord::Config.multi_aggregator_classes.count == 0
-      warn_class_string_deprecation('multi_aggregator_classes')
+      ActiveSupport::Deprecation.warn(
+        "multi_aggregator_classes is deprecated and will be removed in the next major version.
+        Please use multi_aggregators, e.g. [:console, :bugsnag]"
+      )
       BrokenRecord::Config.multi_aggregators = BrokenRecord::Config.multi_aggregator_classes.map do |klass_string|
         get_symbol_from_klass_string(klass_string)
       end
     end
 
     unless BrokenRecord::Config.job_scheduler_class.nil?
-      warn_class_string_deprecation('job_scheduler_class')
+      ActiveSupport::Deprecation.warn(
+        "job_scheduler_class is deprecated and will be removed in the next major version.
+        Please use job_scheduler, e.g. :parallel_job"
+      )
       BrokenRecord::Config.job_scheduler = get_symbol_from_klass_string(BrokenRecord::Config.job_scheduler_class)
     end
   end
 
   private
-
-  def warn_class_string_deprecation(config_option)
-    ActiveSupport::Deprecation.warn(
-      "#{config_option} is deprecated and will be removed in the next major version.
-      Please use #{config_option.gsub('_class', '')}, e.g. :console, :parallel_job"
-    )
-  end
 
   def get_symbol_from_klass_string(klass_string)
     klass_base_name = klass_string.split("::")[-1]
